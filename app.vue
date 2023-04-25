75c16ca254a2081d92341a54490cf19181529075
DESKTOP-ENRHNU6\User <cutecat8110@gmail.com>
2023-04-25 10:26:30 +0800

    chore: 安裝 Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">鐵人賽</p>
+    </div>
   </div>
 </template>
-
-<script setup>
-const year = 2022
-const title = `${year} iThome 鐵人賽`
-</script>
